a1bfb2ffc5e77bd6d84b9ebb339f13e3e5d6f82e
SZ-Aarica <112103155+SZ-Aarica@users.noreply.github.com>
2022-09-05 14:30:41 +0430

    getting API response from search bar

--- script.js
+++ script.js
@@ -26,6 +26,37 @@ date.innerHTML =
   now.getMonth() + 1 + "/" + now.getDate() + "/" + now.getFullYear();
 //-----------------------------------------------------------------
 let description = document.querySelector("#weather");
-let temp = document.querySelector("#temperature");
+let temperature = document.querySelector("#temperature");
 let apiKey = "74e718c96dc64467c77303080c47c11d";
 let humidity = document.querySelector("#humidity");
+
+function showWeather(city) {
+  let Url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(Url).then(function (response) {
+    temperature.innerHTML = Math.round(response.data.main.temp);
+    description.innerHTML = response.data.weather[0].description;
+    humidity.innerHTML = response.data.main.humidity;
+
+    if (response.data.weather[0].description.includes("cloud")) {
+      document.getElementById("image").src = "img/cloudy.svg";
+    } else if (response.data.weather[0].description.includes("rain")) {
+      document.getElementById("image").src = "img/rain.svg";
+    } else if (response.data.weather[0].description.includes("snow")) {
+      document.getElementById("image").src = "img/snow.svg";
+    } else {
+      document.getElementById("image").src = "img/sunny.svg";
+    }
+  });
+}
+
+function showCityName(e) {
+  e.preventDefault();
+  let input = document.querySelector("#search");
+  let cityName = document.querySelector(".city-name");
+  cityName.innerHTML = input.value;
+  showWeather(input.value);
+}
+
+let form = document.querySelector(".search-bar");
+form.addEventListener("submit", showCityName);
